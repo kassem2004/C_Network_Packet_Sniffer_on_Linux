ffe8ffe12d43efcb3c29a649cf2a0707ef6893bb
kassem2004 <kazeinab123@gmail.com>
2024-10-22 20:08:26 +0300

    Made simple sniffer, captures all packets, displays packet size.

--- RawSocket.c
+++ RawSocket.c
@@ -8,5 +8,26 @@
 #include <arpa/inet.h>  
 
 int main(){
-    printf("Hello world!");
+    int socketfd;
+    struct sockaddr saddr;
+    unsigned char *buffer = (unsigned char *) malloc(65536);  // Allocate buffer to hold the packet
+
+    socketfd = socket(AF_PACKET, SOCK_RAW, htons(ETH_P_ALL));
+
+    if(socketfd < 0){
+        perror("Socket error");
+        return 1;
+    }
+
+    int count = 0;
+    int data_size, saddr_size;
+    while(count < 10){
+        saddr_size = sizeof(saddr);
+        data_size = recvfrom(socketfd, buffer, 65536, 0, &saddr, &saddr_size);
+        printf("Captured Packet Size: %d bytes\n", data_size);
+        count++; 
+    }
+
+    close(socketfd);
+    return 0;
 }
