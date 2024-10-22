18177464fcee05d06de843d8c15364982ab8b3e8
kassem2004 <kazeinab123@gmail.com>
2024-10-22 16:53:35 +0300

    Integrated WSL, checking if everything works

---
+++ RawSocket.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/socket.h>
+#include <netinet/ip.h>
+#include <netinet/if_ether.h>
+#include <arpa/inet.h>  
+
+int main(){
+    printf("Hello world");
+}
