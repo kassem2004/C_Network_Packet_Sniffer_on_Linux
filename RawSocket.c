53869d97efa2a8b45d42443f0037853e6aa34242
kassem2004 <kazeinab123@gmail.com>
2024-10-24 16:56:27 +0300

    Commiting to test on Linux VM

--- RawSocket.c
+++ RawSocket.c
@@ -13,11 +13,14 @@
 void ProcessPacket(unsigned char* , int);
 void PrintUDP(unsigned char* , int);
 void PrintTCP(unsigned char* , int);
+void PrintETH(unsigned char* , int);
+void PrintIPH(unsigned char* , int);
+void PrintBUF(unsigned char* , int); //for debugging to help visualize the packet
 
 int main(){
     int socketfd;
     struct sockaddr saddr;
-    unsigned char *buffer = (unsigned char *) malloc(65536);  // Allocate buffer to hold the packet
+    unsigned char *buffer = (unsigned char *) malloc(65536);  // Allocate buffer to hold the packet, buffer currently points at first address of allocated memory
 
     socketfd = socket(AF_PACKET , SOCK_RAW , htons(ETH_P_ALL));
 
@@ -28,11 +31,13 @@ int main(){
 
     int count = 0;
     int data_size, saddr_size;
-    while(count < 10){
+    while(count < 5){
         saddr_size = sizeof(saddr);
-        data_size = recvfrom(socketfd, buffer, 65536, 0, &saddr, &saddr_size);
+        data_size = recvfrom(socketfd, buffer, 65536, 0, &saddr, &saddr_size); //packet stored at where buffer is pointing
         printf("Captured Packet Size: %d bytes\n", data_size);
         count++; 
+        PrintBUF(buffer, data_size);
+        memset(buffer, 0, 65536);
         ProcessPacket(buffer, data_size);
     }
 
@@ -41,19 +46,48 @@ int main(){
 }
 
 void ProcessPacket(unsigned char* buffer, int data_size){
-    struct iphdr *iph = (struct iphdr *) buffer;
-    switch(iph->protocol){
-        case 1:
-            printf("ICMP Packet");
-			break;
-		case 2:
-			printf("IGMP Packet");
-			break;
+    struct iphdr *iph = (struct iphdr *) (buffer + 14); //+14 because I am recieving packets with the L2 Ethernet header, Ethernet header is 14 bytes
+    switch(iph->protocol){                              //I want the L3 IP Header 
         case 6:
-            printf("TCP Packet");
-            break;
-        case 17:
-            printf("UDP Packet");
-            break;
+        PrintTCP(buffer, data_size);
+    }                     
+}
+
+void PrintTCP(unsigned char* buffer, int data_size){
+    printf("TCP Packet\n");
+    //PrintETH(buffer, data_size);
+
+}
+
+void PrintUDP(unsigned char* buffer, int data_size){
+    printf("UDP Packet\n");
+
+}
+
+/*
+void PrintETH(unsigned char* buffer, int data_size){
+    struct ethhdr *eth = (struct ethhdr *) buffer;
+
+    printf("*********************ETH HEADER***********************\n");
+    printf("|-Destination Address : %.2X-%.2X-%.2X-%.2X-%.2X-%.2X \n", eth->h_dest[0] , eth->h_dest[1] , eth->h_dest[2] , eth->h_dest[3] , eth->h_dest[4] , eth->h_dest[5]);
+    printf("|-Source Address : %.2X-%.2X-%.2X-%.2X-%.2X-%.2X \n", eth->h_source[0] , eth->h_source[1] , eth->h_source[2] , eth->h_source[3] , eth->h_source[4] , eth->h_source[5]);
+    printf("|-Protocol : %u\n", eth->h_proto);
+    printf("******************************************************\n");
+}
+*/
+
+void PrintIPH(unsigned char* buffer, int data_size){
+    printf("TCP Packet\n");
+
+}
+
+void PrintBUF(unsigned char* buffer, int size) {
+    printf("Buffer content (hex):\n");
+    for(int i = 0; i < size; i++) {
+        printf("%.2X ", buffer[i]);
+        if ((i + 1) % 16 == 0) {
+            printf("\n");
+        }
     }
+    printf("\nEnd of buffer content\n");
 }
